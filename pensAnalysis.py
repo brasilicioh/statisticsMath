a4c86e72881ddac52fb2a92d3e20970a8fff6db5
brasilicioh <brasiliciohcr@gmail.com>
2025-07-22 23:12:25 -0300

    first commit

---
+++ pensAnalysis.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Analisar todos os itens vendidos em pens.csv e começar a montar gráficos e relações estatísticas.
---
+++ salesAnalysis.py
@@ -0,0 +1,4 @@
+import re
+
+# Ler sales.txt e analisar todas as vendas de canetas
+# Colocar em pens.csv todas as vendas de canetas de forma organizadas.
