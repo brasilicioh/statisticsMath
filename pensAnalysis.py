7ce08c1c4e22a24a6a2123feda1143aa2e8c4c8e
Kaio Henrique <kaio.lopes@escolar.ifrn.edu.br>
2025-07-27 16:35:16 -0300

    smo graphic

--- pensAnalysis.py
+++ pensAnalysis.py
@@ -3,3 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Analisar todos os itens vendidos em pens.csv e começar a montar gráficos e relações estatísticas.
+
+csv = pd.read_csv("items.csv")
+
+csv[" Preço und/"] = csv[" TotalVenda"] / csv[" Quantidade"]
+
+
+print(csv)
+
+
+#sessão gráfico
+plt.figure(figsize=(10, 6))
+plt.bar(csv["Descricao"], csv[" Preço und/"], color="skyblue")
+plt.xlabel("Produto")
+plt.ylabel("Preço por Unidade")
+plt.title("Preço por Unidade de Cada Produto")
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
