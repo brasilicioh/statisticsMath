7dec8da29edebc417935150e155241f8181b8c4d
brasilicioh <brasiliciohcr@gmail.com>
2025-07-27 12:41:13 -0300

    Add arquivos básicos e código de transformar txt em csv

--- salesAnalysis.py
+++ salesAnalysis.py
@@ -1,4 +1,23 @@
 import re
+import re
+import csv
 
-# Ler sales.txt e analisar todas as vendas de canetas
-# Colocar em pens.csv todas as vendas de canetas de forma organizadas.
+with open("sales.txt", "r", encoding="utf-8") as file:
+    linhas = file.readlines()
+
+informacoes = []
+
+regex = re.compile(r"\|\s*([A-Z0-9\s\-\.]+?)\s*\|\s*([\d,.]+)\|\s*([\d,.]+)\|")
+
+for linha in linhas:
+    if regex.search(linha):
+        descricao = regex.search(linha).group(1).strip()
+        quantidade = regex.search(linha).group(2).replace(".", "").replace(",", ".")
+        total_venda = regex.search(linha).group(3).replace(".", "").replace(",", ".")
+        if descricao.split()[0]:
+            informacoes.append([descricao, float(quantidade), float(total_venda)])
+
+with open("items.csv", "w", newline='', encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Descricao", "Quantidade", "TotalVenda"])
+    writer.writerows(informacoes)
